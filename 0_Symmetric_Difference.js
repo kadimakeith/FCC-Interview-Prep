d3a53046da9108a0f574f39e380cb6494d6fd2e2
Akashik Overlord <keithkadima@gmail.com>
2022-12-03 15:52:23 +0300

    Create 0_Symmetric_Difference.js

---
+++ 0_Symmetric_Difference.js
@@ -0,0 +1,15 @@
+function sym(args1, args2) {
+    let v = args1.filter((el) => 
+       !args2.includes(el)
+    )
+    
+    let w = args2.filter((el) =>
+       !args1.includes(el)
+    )
+
+   let answer = v.concat(w)
+   let uniqanswer = [...new Set(answer)]
+   return uniqanswer
+}
+
+//TO DO - Implement for N arguments
