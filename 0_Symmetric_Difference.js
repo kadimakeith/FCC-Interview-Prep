283c1298b31259fae07312fa17ef34cc85b5e894
Akashik Overlord <keithkadima@gmail.com>
2022-12-06 15:53:31 +0300

    Update 0_Symmetric_Difference.js

--- 0_Symmetric_Difference.js
+++ 0_Symmetric_Difference.js
@@ -1,17 +1,39 @@
-function sym(args1, args2) {
-    let v = args1.filter((el) => 
-       !args2.includes(el)
-    )
-    
-    let w = args2.filter((el) =>
-       !args1.includes(el)
-    )
+/**
+ * Author: Keith K. Kadima
+ *
+ * Description:
+ *
+ * function that takes two or more arrays and returns
+ * an array of their symmetric difference. The returned array must contain only unique values (no duplicates).
+ *
+ * The mathematical term symmetric difference (△ or ⊕) of two sets is the set of elements which are in either of the two sets but not in both. 
+ * For example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}.
+ *
+ */
 
-   let answer = v.concat(w)
-   
-   //Remove duplicates
-   let uniqanswer = [...new Set(answer)]
-   return uniqanswer
+
+function sym() {
+    let args = []
+
+    for (let i = 0; i < arguments.length; ++i){
+       args.push(arguments[i])
+     }
+
+ 
+    function symdiff(args1, args2){
+        let v = args1.filter((el) => 
+        !args2.includes(el)
+        )
+        
+        let w = args2.filter((el) =>
+        !args1.includes(el)
+        )
+
+        let answer = v.concat(w)
+        let uniqanswer = [...new Set(answer)]
+        return uniqanswer
+    } 
+
+    return args.reduce(symdiff)
+ 
 }
-
-//TO DO - Implement for N arguments
