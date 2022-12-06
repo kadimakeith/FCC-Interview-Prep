e6d601a02d8bbfb81d29ba11323bce3eb6504a9f
Akashik Overlord <keithkadima@gmail.com>
2022-12-06 16:10:42 +0300

    update description

---
+++ 1-Inventory_Update.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Keith K. Kadima
+ *
+ * Description:
+ *
+ * Compares and updates the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+ * Updates the current existing inventory item quantities (in arr1). 
+ * If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+ *
+ */
